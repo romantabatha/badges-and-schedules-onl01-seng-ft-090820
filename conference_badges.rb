6c671e56290064ecc71360543548625ed17afe30
Tabatha Roman <romantabatha17@gmail.com>
2020-09-10 13:42:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def batch_badge_creator
+  attendees.each {|name| puts "Hello, my name is #{name}"}
+end
+
+def assign_rooms
+  rooms = [1,2,3,4,5,6,7]
+  puts "Hello, #{attendees.index}! You'll be assigned to room #{rooms.index}!"
+end
