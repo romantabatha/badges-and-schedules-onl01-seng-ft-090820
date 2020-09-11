9fe5c85ecea072cd82a30c5f00c467eb5fe720ef
Tabatha Roman <romantabatha17@gmail.com>
2020-09-11 19:02:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,15 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
- attendees.each {|name|"Hello, my name is #{name}"}
+  new_array =[]
+  #attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+ attendees.each do |name|
+  new_array << "Hello, my name is #{name}" 
+ end
+ new_array
 end
 
 def assign_rooms(rooms)
-  rooms = [1,2,3,4,5,6,7]
+  #rooms = [1,2,3,4,5,6,7]
  "Hello, #{attendees.index}! You'll be assigned to room #{rooms.index}!"
 end
