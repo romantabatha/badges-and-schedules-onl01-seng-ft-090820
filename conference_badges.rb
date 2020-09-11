e12e3bd96730a1916c6f7635ea98455e033c3d41
Tabatha Roman <romantabatha17@gmail.com>
2020-09-11 19:35:16 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,26 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+ "Hello, my name is #{name}."
 end
 
-attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-
-def batch_badge_creator
-  attendees.each {|name| puts "Hello, my name is #{name}"}
+def batch_badge_creator(attendees)
+#   new_array =[]
+#   #attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+# attendees.each do |name|
+#   new_array << "Hello, my name is #{name}." 
+# end
+# new_array
+  attendees.map {|name|badge_maker(name)}
 end
 
-def assign_rooms
-  rooms = [1,2,3,4,5,6,7]
-  puts "Hello, #{attendees.index}! You'll be assigned to room #{rooms.index}!"
+def assign_rooms(attendees)
+  new_array =[] 
+  attendees.each_with_index do |name,i|
+  new_array << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+ end
+ new_array
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|badge| puts badge}
+  assign_rooms(attendees).each{|room_assignment| puts room_assignment}
 end
